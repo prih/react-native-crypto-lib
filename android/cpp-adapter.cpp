6b917025fd358041d69b7c2521d3ae20011bb438
prih <prih@prihhost.com>
2021-07-16 18:59:59 +0300

    randomBytes changes

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -11,12 +11,12 @@ Java_com_reactnativecryptolib_CryptoLibModule_randomNumber(JNIEnv *env, jclass t
 extern "C"
 JNIEXPORT jbyteArray JNICALL
 Java_com_reactnativecryptolib_CryptoLibModule_randomBytes(JNIEnv *env, jclass type, jint length) {
-  uint8_t bytes[length];
-  random_buffer(bytes, length);
+  jbyte bytes[length];
+  random_buffer(reinterpret_cast<uint8_t*>(bytes), length);
 
   jbyteArray result;
   result = env->NewByteArray(length);
-  env->SetByteArrayRegion(result, 0, length, reinterpret_cast<const jbyte*>(bytes));
+  env->SetByteArrayRegion(result, 0, length, bytes);
 
   return result;
 }
